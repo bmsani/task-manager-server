31479991381d22b7c96291bc5a4a94ac5e9f43c3
bmsani <bmsani6@gmail.com>
2022-07-01 10:47:27 +0600

    api added

--- index.js
+++ index.js
@@ -19,7 +19,41 @@ async function run() {
         await client.connect();
         const taskCollection = client.db('todo_task').collection('task');
 
+        app.get('/task', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const result = await taskCollection.find(query).toArray()
+            res.send(result)
+        })
 
+        app.post('/task', async (req, res) => {
+            const task = req.body;
+            // const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const result = await taskCollection.insertOne(task);
+            res.send({ success: true, result });
+        })
+
+        app.put('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const taskDone = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedTask = {
+                $set: {
+                    done: taskDone.done
+                }
+            };
+            const result = await taskCollection.updateOne(filter, updatedTask, options);
+            res.send({ success: true, result });
+        })
+
+        // delete
+        app.delete('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
 
